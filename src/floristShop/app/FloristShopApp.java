2221cd12a5316cfb0ade5c2c67494d7c76c865ca
RoseVar <roservargas@gmail.com>
2020-10-08 08:53:52 +0200

    First commit: creation

---
+++ src/floristShop/app/FloristShopApp.java
@@ -0,0 +1,11 @@
+package floristShop.app;
+
+public class FloristShopApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Por hacer");
+
+	}
+
+}

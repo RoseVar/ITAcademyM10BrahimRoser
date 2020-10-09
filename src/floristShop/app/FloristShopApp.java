b10af82a727cad646c96beb6c6a80c337a3978e0
ibra1998 <taza-brahim@hotmail.es>
2020-10-10 01:40:25 +0200

    Lo hago porque me lo pide
    Al parecer hay algunos archivos que no se han guardado sus cambios, y
    por ello, debo hacer esre commit

--- src/floristShop/app/FloristShopApp.java
+++ src/floristShop/app/FloristShopApp.java
@@ -1,38 +1,10 @@
 package floristShop.app;
 
-import floristShop.views.MainFrame;
-
 public class FloristShopApp {
 
 	public static void main(String[] args) {
-		
-	    //TODO declarar e instanciar el modelo: myModel
-	    //MainFrame myFrame= new MainFrame(null); //TODO pasarle el modelo de datos
-	    //myFrame.setVisible(true); 
-	    
-	    FloristShopApp myFloristManager = new FloristShopApp();
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                //call our method to start
-            	myFloristManager.run();
-            }
-        });
-	}
-        
-                
-
-    /**
-     * Method to star running our application.
-     */
-    private void run() {
-        //Instantiate model and main frame
-        //myModel
-        MainFrame myFrame = new MainFrame(null);
-        //Put it in the center of screen
-        myFrame.setLocationRelativeTo(null);
-        //set visible the main frame
-        myFrame.setVisible(true);              
+		// TODO Auto-generated method stub
+		System.out.println("Por hacer");
 
 	}
 
